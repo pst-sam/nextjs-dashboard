b11cf918bba6342438c40bf808b4459b503ed222
Samuel Afolabi <1samuelafolabi@gmail.com>
2024-12-18 08:31:52 +0100

    Final Chapter 16

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import {
   fetchLatestInvoices,
   fetchCardData,
 } from "@/app/lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function Page() {
   const revenue = await fetchRevenue();
